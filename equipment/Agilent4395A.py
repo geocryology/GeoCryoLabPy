40ec20bd03f1f78f6c866ddc8c383b96f9f43c97
A139 <A139>
2016-09-01 19:13:13 +0000

    added some comments

--- equipment/Agilent4395A.py
+++ equipment/Agilent4395A.py
@@ -1,37 +1,56 @@
 import visa
 
+# Controller class for the Agilent 4395A Frequency Response Analyzer
+#   - operates over GPIB using a National Instruments USB-GPIB cable
+#
+# GPIB uses two methods for communication:
+#   WRITE - Sends a command, doesn't return a response
+#   QUERY - Sends a command, followed by a '?', and returns a response
+
+
 class Agilent4395A:
 
+    # GPIB Address, used to specify connection
+    ADDRESS = u'GPIB0::16::INSTR'
+    
+    # GPIB ID string, returned by the '*IDN?' query, used to test if connection is successful
     ID = u'HEWLETT-PACKARD,4395A,MY41101925,REV1.12\n'
 
     def __init__(self):
         self.rm = visa.ResourceManager()
         self.analyzer = None
         
+    # Connect to and initialize the analyzer
     def connect(self):
         
-        self.analyzer = self.rm.open_resource(u'GPIB0::16::INSTR')
-        id = (self._query("*IDN?"))
+        self.analyzer = self.rm.open_resource(self.ADDRESS)
+        id = (self.query("*IDN?"))
         if id != self.ID:
             print "ID discrepancy:"
-            print "expected:", self.ID
-            print "actual:  ", id
+            print "  expected:", self.ID
+            print "  actual:  ", id
             return False
 
         self._write("*RST")
         self._write("*CLS")
         return True
         
+    # Close the connection (should be done before script exits)
     def disconnect(self):
         self.analyzer.close()
         
-    def _write(self, cmd):
-        return self.analyzer.write(cmd)
+    # Sends a string to the analyzer, does not return a response
+    def write(self, cmd):
+        self.analyzer.write(cmd)
         
-    def _query(self, cmd):
+    # Sends a string (must end with '?'), returns response
+    # If the response is large, it may take several seconds to return
+    def query(self, cmd):
         return self.analyzer.query(cmd)
         
 if __name__ == "__main__":
+    
+    # Test script, sends some configuration commands and reads the measured data
     
     import time
     
@@ -56,7 +75,8 @@ if __name__ == "__main__":
     for cmd in commands:
         print cmd.strip(),
         raw_input()
-        fra._write(cmd.strip())
-    
+        fra.write(cmd.strip())
+        
+    data = fra.query("OUTPDTRC?")
     
     fra.disconnect()
