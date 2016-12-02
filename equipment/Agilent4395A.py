484179f0f4d3d4db2dad2e6a85f1c286d4ccdc6a
stokastic <aaron_hill2@carleton.ca>
2016-12-02 22:19:54 +0000

    Adding most recent changes

--- equipment/Agilent4395A.py
+++ equipment/Agilent4395A.py
@@ -1,3 +1,6 @@
+import cmath
+import datetime
+import sys
 import visa
 
 # Controller class for the Agilent 4395A Frequency Response Analyzer
@@ -31,8 +34,8 @@ class Agilent4395A:
             print "  actual:  ", id
             return False
 
-        self._write("*RST")
-        self._write("*CLS")
+        self.write("*RST")
+        self.write("*CLS")
         return True
         
     # Close the connection (should be done before script exits)
@@ -56,27 +59,136 @@ if __name__ == "__main__":
     
     fra = Agilent4395A()
     
+    
     if not fra.connect():
         print "Failed to connect to Agilent4395A"
 
-    commands = """CHAN1
+    # dictionaries to store measurements
+    results = {"A": {}, "B": {}}
+    channels = ["A", "B"]
+    filename = sys.argv[1]
+    
+    # Setup parameters
+    Rsa = 50.1 # resistance, ohms
+    Rsb = 50.1 #
+    power = 15.0 # dB
+    nPoints = 801
+    f1 = 1000
+    f2 = 500000000
+    
+    # Validate parameters
+    if not (1 <= nPoints <= 801):
+        print "nPoints must be in the range [0, 801]"
+        exit()
+    
+    if not (f1 < f2):
+        print "f1 must be less than f2"
+        exit()
+        
+    if not (10 < f1 < 510000000):
+        print "start/stop frequencies must be in the range [10, 510M]"
+        exit()
+        
+    if not (10 < f2 < 510000000):
+        print "start/stop frequencies must be in the range [10, 510M]"
+        exit()
+        
+    
+    fmts = ["LINM", "PHAS", "REAL", "IMAG", "LOGM", "DELA", "SWR", "SMITH", "POLA", "ADMIT"]
+    commands = """NA
+        CHAN1
+        HOLD
         SWPT LOGF
         BWAUTO 1
-        POIN 801
+        POIN {}
         FORM4
-        MEAS A
+        MEAS {{}}
         PHAU DEG
-        STAR 1000 HZ
-        STOP 500000000 HZ
-        FMT LINM
-        SING
-        AUTO""".split("\n")
+        STAR {} HZ
+        STOP {} HZ
+        POWE {}
+        FMT LINM""".format(nPoints, f1, f2, power)
+    
+    for channel in channels:
+        #print "press enter to measure channel {}".format(channel), raw_input()
+    
+        # Configure analyzer for measurements
+        print "Configuring analyzer for measurement of channel {}".format(channel)
+        commandList = commands.format(channel).split("\n")
+        for cmd in commandList:
+            print "  {}".format(cmd.strip())
+            fra.write(cmd.strip())
+        time.sleep(15)
         
-    for cmd in commands:
-        print cmd.strip(),
-        raw_input()
-        fra.write(cmd.strip())
+        # Get sweep duration
+        t = 10
+        try:
+            duration = fra.query("SWET?").strip()
+            t = float(duration)
+        except:
+            print "failed to convert to float: ", duration
+        print "sweep time: {}".format(t)
         
-    data = fra.query("OUTPDTRC?")
+        # Make measurement
+        t0 = time.time()
+        fra.write("SING")
+        while time.time() < t0 + t + 5:
+            print "waiting"
+            time.sleep(1)
+            
+        # Read data from analyzer
+        for fmt in fmts:
+            print "Reading Channel {}: {} ...".format(channel, fmt),
+            fra.write("FMT {}".format(fmt))
+            
+            # results are read as list of x1,y1,x2,y2,x3,y3... where every yn value is 0. 
+            # this line splits the list at every comma, strips out every second value, and converts to floats
+            response = fra.query("OUTPDTRC?")
+            print "done - {} bytes".format(len(response))
+            results[channel][fmt] = map(float, response.strip().split(",")[::2])
+            
+    # Read x-axis values (frequency points)
+    freqs = fra.query("OUTPSWPRM?").strip()
+    freqs = map(float, freqs.split(","))
+    
+    timestamp = datetime.datetime.now().isoformat()
+    
+    print "saving file"
+    file = open("sweepResults_{}.csv".format(filename), "w")
+    file.write("Impedance Measurement Performed with an Agilent 4395A Network Analyzer\n")
+    file.write("File generated on: {}\n".format(timestamp))
+    file.write("Rs = {} ohms\n".format(Rsb))
+    file.write("Impedance Calculation: Rs x (Va - Vb) / Vb\n")
+    file.write("Start Frequency: {} Hz\n".format(f1))
+    file.write("Stop  Frequency: {} Hz\n".format(f2))
+    file.write("Number of data points: {}\n".format(nPoints))
+    file.write("Source Power (dB): {}\n".format(power))
+    file.write("Measurement BW: auto \n")
+    file.write("\n") # Store additional info here
+    file.write("\n") # Store additional info here
+    file.write("Frequency,Va (real),Va (imag),Vb (real),Vb (imag),Va Mag,Va Phase,Vb Mag,Vb Phase,Impedance Mag,Impedance Phase\n")
+    
+    for i in range(nPoints):
+        freq   = freqs[i]
+        VaReal = results["A"]["REAL"][i]
+        VaImag = results["A"]["IMAG"][i]
+        VbReal = results["B"]["REAL"][i]
+        VbImag = results["B"]["IMAG"][i]
+        Va     = VaReal + 1j * VaImag
+        Vb     = VbReal + 1j * VbImag
+        Z      = Rsa * (Va - Vb) / Vb
+        VaMag, VaPhase = cmath.polar(Va)
+        VbMag, VbPhase = cmath.polar(Vb)
+        ZMag, ZPhase   = cmath.polar(Z)
+        VaPhase        = 180 * VaPhase / cmath.pi
+        VbPhase        = 180 * VbPhase / cmath.pi
+        ZPhase         = 180 * ZPhase / cmath.pi
+
+        file.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(
+            freq, VaReal, VaImag, VbReal, VbImag,
+            VaMag, VaPhase, VbMag, VbPhase, ZMag, ZPhase))
+        
+    file.close()
     
     fra.disconnect()
+    exit()
