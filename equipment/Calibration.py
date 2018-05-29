933109613cf0396ef8cbfca2f1c3cf6dfe3909a6
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2018-05-29 13:43:36 -0400

    Add files via upload
    Now simplified by using pandas

--- equipment/Calibration.py
+++ equipment/Calibration.py
@@ -15,9 +15,11 @@ from statistics import median
 import operator
 import uncertainties as unc   # https://pypi.org/project/uncertainties/
 import uncertainties.unumpy as unp
+import pandas as pd 
 
 # Input
-file = '2018-05-23T13_54_10_avg.csv'   #'2018-05-18T20_39_23_avg_2'
+dir = 'C:/Users/estew/Documents/2018SummerStephan/CalibFiles/'   # Directory of input csv and where outputted csv files will go
+file = '2018-05-23T13_54_10_avg.csv'                             # Name of input csv file
 point = "0.0"
 
 # Define function to fit 
@@ -25,66 +27,67 @@ def func(x, a, b, c, d):                      # Where x is Rt/R0 and a, b, c and
     return 1/(a + b * np.log(x) + c * (np.log(x))**2 + d * (np.log(x))**3)
 
 # Import data
-with open(file, 'rb') as file:
-    reader = csv.reader(file, dialect='excel', delimiter =',')
-    data_r = np.array(list(reader))
-    names = list(data_r[0, 4:])
-    data = map(list, zip(*data_r[1:]))
-    time = data[0][0]
-    time = [time[:10]] * len(names)
-    SetPoint = list(data[1])
-    ProbeTemp = data[2]
-    ThermRes = data[4:]
+table = pd.read_csv(dir + file)
+ThermRes = table.iloc[:, 4:]
         
 # Prepares data for plotting
-ind = int(SetPoint.index(point))
+ind = table[table['Setpoint']==0.0].index.values.astype(int)[0]
 # preparing thermistor resistance data (xdata)
 xdata = []
 for therm in ThermRes:
-    res = map(float, therm)
+    res = ThermRes[therm]             #map(float, ThermRes[therm])
     res = [i/res[ind] for i in res]   # Resistance divided by resistance at 25 degrees C
     xdata.append(res)
 
 # preparing probe temperature data (ydata)
-ydata = map(float, ProbeTemp)          # Probe temperature is the y axis data
+ydata = table['ProbeTemp']         # Probe temperature is the y axis data
 ydata = [i+273.15 for i in ydata]      # Convert celcius to kelvin
 
 # Fit curves to function, calculates uncertainties, residuals and prepares plots
-par = []             # List for results csv
-stats = []           # List for stats csv
-for x in range(0, len(xdata)):
+results = pd.DataFrame()
+stats = pd.DataFrame()           # Create empty pandas dataframe
+for i in range(0, len(xdata)):
+    x = xdata[i]
     # Fitting curve
-    popt, pcov = curve_fit(func, xdata[x], ydata, bounds=(0, [1.0e-1, 1.0e-2, 1.0e-4, 1.0e-5]))  # popt is the parameter values and pvoc is the vovariance matrix
-   
-   # Calculating uncertainties
+    popt, pcov = curve_fit(func, x, ydata, bounds=(0, [1.0e-1, 1.0e-2, 1.0e-4, 1.0e-5]))  # popt is the parameter values and pvoc is the vovariance matrix
+    
+    # Calculating uncertainties
     a, b, c, d = unc.correlated_values(popt, pcov)     # Calculates +/- uncertainty of parameters using sqrt of diagonal of pcov. http://scipyscriptrepo.com/wp/?p=104
-    py = 1/(a + b * unp.log(xdata[x]) + c * (unp.log(xdata[x]))**2 + d * (unp.log(xdata[x]))**3)   # calculates the temp with uncertainties for every temp step
+    py = 1/(a + b * unp.log(x) + c * (unp.log(x))**2 + d * (unp.log(x))**3)   # calculates the temp with uncertainties for every temp step
     nom = unp.nominal_values(py)        # Creates array of temp value at each temp step
     std = unp.std_devs(py)              # Creates array of uncertainty (1 standard deviation) at each temp step
     std_mma = [median(std *3.92), max(std)*3.92, min(std)*3.92]  # Calculates mode, max and min of the uncertainties over all temps of thermistor for csv file
     
     # Calculating residuals
-    dif = func(xdata[x], *popt) - ydata   # Calculates difference between curve and data points
+    dif = func(x, *popt) - ydata   # Calculates difference between curve and data points
     dif_mma = [np.mean(abs(dif)), max(dif), min(dif)]    # Calculates mean, max and min residual over the temp range of a thermistor for csv file
     # Calculate standard uncertainty (1.96 sigma: 95%, 1.65 sigma: 90%)
     cov = np.sqrt(np.diag(pcov)) * 1.96  # Not used anymore as unc.correlated_values does this. But still used for stats csv
-    
-    # Prepare data for stats csv
-    stat = [time[x]] + [names[x]] + list(popt) + list(cov) + list(std_mma) + list(dif_mma)   
-    stats.append(stat)
+
     # Prepare data for results csv
-    tab = [time[x]] + [names[x]] + list(popt) + [std_mma[0], dif_mma[0]] 
-    par.append(tab)
+    result = pd.DataFrame({'Date': [table["Time"][0][:10]], 
+                        'Thermistor': [ThermRes.columns.values[i]],
+                        'a': popt[0], 'b': popt[1], 'c': popt[2], 'd': popt[3],
+                        'std_median': std_mma[0], 'err_avg': dif_mma[0]})
+    results = results.append(result, ignore_index=True)
+    # Prepare data for stats csv
+    stat = pd.DataFrame({'Date': [table["Time"][0][:10]], 
+                        'Thermistor': [ThermRes.columns.values[i]],
+                        'a': popt[0], 'b': popt[1], 'c': popt[2], 'd': popt[3],
+                        'stu_a': cov[0], 'stu_b': cov[1], 'stu_c': cov[2], 'stu_d': cov[3],
+                        'std_median': std_mma[0], 'std_max': std_mma[1], 'std_min': std_mma[2],
+                        'err_avg': dif_mma[0], 'err_max': std_mma[1], 'err_min': std_mma[2]})
+    stats = stats.append(stat, ignore_index=True)
     
     ## PLOTTING
     # Plot Curves and points
     plt.subplot(311)
-    plt.plot(xdata[x], func(xdata[x], *popt), 'g-')
+    plt.plot(x, func(x, *popt), 'g-')
     plt.ylabel('Temperature (k)')
-    plt.plot(xdata[x], ydata, 'c.', markersize = 1)
+    plt.plot(x, ydata, 'c.', markersize = 1)
     # Plotting residuals
     plt.subplot(312)
-    plt.plot(xdata[x], dif, 'r.', markersize = 2)
+    plt.plot(x, dif, 'r.', markersize = 2)
     plt.xlabel('Rt/R0')
     plt.ylabel('Error (K)') 
     # Plot T uncertainty over T range
@@ -93,22 +96,11 @@ for x in range(0, len(xdata)):
     plt.xlabel('Temperature (K)')
     plt.ylabel('Uncertainty (K)')
    
-
-# Write uncertainties csv (not currently working)
-with open("stats.csv", "w") as output:
-    output.write("Date, Thermistor, a, b, c, d, stu_a, stu_b, stu_c, stu_d, std_median, std_max, std_min, err_avg, err_max, err_min\n")
-
-with open('stats.csv', 'a') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-    writer.writerows(stats)    
-
 # Write results csv
-with open("results.csv", "w") as output:
-    output.write("Date, Thermistor, a, b, c, d, std_median, err_avg\n")
+results.to_csv(dir + 'results.csv', index = False)
 
-with open('results.csv', 'a') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-    writer.writerows(par)
+# Write stats csv
+stats.to_csv(dir + 'stats.csv', index = False)   
     
 # Plot data and save file
 plt.tight_layout()
