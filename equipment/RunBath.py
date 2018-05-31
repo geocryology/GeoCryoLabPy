65c87c633aeb082db79623872b0808f60c016cd4
A139 <A139@PCLAA139-SG.CUNET.CARLETON.CA>
2018-05-31 20:55:56 +0000

    usage examples for Lauda

--- equipment/RunBath.py
+++ equipment/RunBath.py
@@ -7,28 +7,45 @@ from time import sleep
 delay = 30 ## delay n seconds before starting
 
 # conect to bath
-bath = LaudaRP845
+bath = LaudaRP845()
 if not bath.connect():
     print("Failed to connect to Lauda")
     exit(1)
 
-# define temperature ramp
-# temps = read_csv()
-time = range(0,180,30)
-temps = [20, 19.5, 19, 18.5, 19, 19.2]
-t_delta = np.diff(time)
+# print(bath.getCurrentProgram())
+# ft = lambda t: 3 * np.sin(2 * t * np.pi / 10) + 15
+# fp = lambda x: 8
+# fto = lambda x: 0.1 if x == 1 else 0
+# bath.setProgramFunction(2, ft, 10, 1, reps = 2, f_pump = fp, f_tol=fto )
 
-# put it to the starting target temp and wait
-bath.setSetpoint(temps[0])
-sleep(30)
+# bath.controlProgram('start')
 
-# start the clock and begin changing temperatures
-for i in range(0,len(time)):
-    bath.setSetpoint(temps[i + 1])
-    cur_set = bath.getSetpoint()
-    cur_temp = bath.getBathTemp()
-    print('target: {}, current: {}'.format(cur_set, cur_temp))
-    sleep(t_delta[i]) # wait until next defined temperature
+print(np.array(bath.getAllProgramSegments(program=2)))
+#bath.controlProgram('start')
+# print('getting programs')
+# print(bath.getAllProgramSegments(program=1))
+
+#bath.setProgramSegment(18, 4, 0,3)
+#print(bath.getCurrentProgram())
+# bath.setPumpLevel(4)
+################################################
+# # define temperature ramp
+# # temps = read_csv()
+# time = range(0,180,30)
+# temps = [20, 19.5, 19, 18.5, 19, 19.2]
+# t_delta = np.diff(time)
+
+# # put it to the starting target temp and wait
+# bath.setSetpoint(temps[0])
+# sleep(30)
+
+# # start the clock and begin changing temperatures
+# for i in range(1, len(time)):
+#     bath.setSetpoint(temps[i])
+#     cur_set = bath.getSetpoint()
+#     cur_temp = bath.getBathTemp()
+#     print('target: {}, current: {}'.format(cur_set, cur_temp))
+#     sleep(t_delta[i - 1]) # wait until next defined temperature
 
 bath.disconnect()
 
