1221b1dc3f3925891ca0f20e5814b0f23e6d77c4
A139 <A139@PCLAA139-SG.CUNET.CARLETON.CA>
2018-07-13 15:53:38 +0000

    scripts directory for common use-cases of python files to not clutter /experiments

---
+++ scripts/process_experiment.py
@@ -0,0 +1,25 @@
+import sys
+from os import path
+
+
+if __name__ == '__main__':
+
+    fp = path.dirname(path.realpath(__file__))
+    eqp = path.join(path.dirname(fp), "equipment")
+    sys.path.append(eqp)
+
+    from ColumnUtils import ColumnExperiment
+    import argparse
+    import configparser
+
+    parser = argparse.ArgumentParser(description="Process raw data from column experiment",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--sh',  type=float, help="soil height in cm")
+    parser.add_argument('--rd',  type=str,   help="path to raw temperature data file")
+    parser.add_argument('--cfg', type=str,   help="path to columnconfig directory")
+
+    args = parser.parse_args()
+
+    C = ColumnExperiment(raw_data = args.rd, cfg_dir = args.cfg, soil_height = args.sh)
+    C.processColumn()
