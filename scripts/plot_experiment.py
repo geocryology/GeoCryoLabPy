e19989a611f11c8fa3d469121ef15efd5a4ee603
A139 <A139@PCLAA139-SG.CUNET.CARLETON.CA>
2018-07-18 19:03:31 +0000

    script to access columnplot from commandline

---
+++ scripts/plot_experiment.py
@@ -0,0 +1,32 @@
+import sys
+from os import path
+
+
+if __name__ == '__main__':
+
+    fp = path.dirname(path.realpath(__file__))
+    eqp = path.join(path.dirname(fp), "equipment")
+    sys.path.append(eqp)
+
+    from ColumnPlot import ColumnPlotter
+    import argparse
+    import configparser
+
+    parser = argparse.ArgumentParser(description="Process raw data from column experiment",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--data',    type=str,   help="path to processed data file")
+    parser.add_argument('--dir',    type=str,   help="directory to save plots")
+    parser.add_argument('--mean',    action='store_true',   help="make a mean plot")
+    parser.add_argument('--cont',    action='store_true',   help="make a contour plot")
+
+    args = parser.parse_args()
+
+    P = ColumnPlotter(args.data)
+
+    if args.mean:
+        P.meanPlot()
+
+    if args.cont:
+        P.contourPlot()
+
